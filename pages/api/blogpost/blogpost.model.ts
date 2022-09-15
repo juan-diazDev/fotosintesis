459e5f7b7e6ecc3b8cb65c03205c673d80ae9928
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-14 21:28:17 -0500

    feat: create blogpost schema V1

---
+++ pages/api/blogpost/blogpost.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  headerImage: {
+    type: String,
+  },
+  likes: {
+    type: String,
+  },
+
+
+}, { versionKey: false, timestamps: true });
+
+const Blogpost = model('blogpost', PostSchema);
+
+export default Blogpost;
