3daa16581822546fb6553e1254e51d46947b4e87
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-14 20:56:26 -0500

    feat: create User Schema in mongoose

---
+++ pages/api/users/user.model.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  location: {
+    country: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+  },
+  blogpost: [{
+    type: Schema.Types.ObjectId,
+    ref: 'blogpost',
+    required: true,
+  }],
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  followers: [{
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  }],
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  }],
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+}, { versionKey: false, timestamps: true });
+
+const User = model('user', UserSchema);
