e373ac3f27c40fc3a7cffc360f625e1f66c7e780
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-15 22:42:04 -0500

    feat: creat verify route

---
+++ pages/api/auth/local/verify.ts
@@ -0,0 +1,38 @@
+import { findOneUser } from "../../../../server/users/user.services";
+import { signToken } from "../../../../server/auth/local/auth.services";
+
+export async function verifyAccountHandler(req, res) {
+  const { token } = req.params;
+  try {
+    const user = await findOneUser({ passwordResetToken: token });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Invalid token' });
+    }
+
+    if (Date.now() > user.passwordResetExpires) {
+      return res.status(404).json({ message: 'Token expired' });
+    }
+
+    user.passwordResetToken = null;
+    user.passwordResetExpires = null;
+    user.isVerified = true;
+
+    await user.save();
+
+    const jwtoken = signToken({ email: user.email });
+
+    return res.status(200).json({
+      token: jwtoken,
+      profile: {
+        email: user.email,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      }, // bring profile
+      message: 'Account activated',
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+}
