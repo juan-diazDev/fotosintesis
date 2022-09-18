679cc2c4bff0218bd038c77f56f2afdc5b46ead4
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-17 23:52:27 -0500

    feat: modify create service and add [id] methods in user module

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
-import connectDB from '../../../server/config/database';
 import crypto from 'crypto';
+import connectDB from '../../../server/config/database';
 import { createUser } from '../../../server/users/user.services';
 
 export default async (req, res) => {
   await connectDB();
-  const userData = req.body
+  const userData = req.body;
 
   try {
     const hash = crypto.createHash('sha256')
@@ -17,11 +17,11 @@ export default async (req, res) => {
     const user = await createUser(userData);
 
     if (req.method !== 'POST') {
-      return res.status(405).json({'[ERROR]': 'Method not allowed'});
+      return res.status(405).json({ '[ERROR]': 'Method not allowed' });
     }
 
-    return res.status(200).json({ user })
+    return res.status(200).json({ user });
   } catch (error) {
-    return res.status(400).json({ Error: error.message})
+    return res.status(400).json({ Error: error.message })
   }
-}
+};
