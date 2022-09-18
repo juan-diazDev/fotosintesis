581126202b35c643dd04051a154bda06e765b697
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-18 14:33:28 -0500

    feat: render created Post from DB SSR

--- components/PostHistory/index.tsx
+++ components/PostHistory/index.tsx
@@ -1,138 +1,30 @@
 import styles from '../../styles/PostHistory.module.scss';
 
-const PostHistory = () => {
+const PostHistory = ({ post }) => {
   return (
     <main className={styles.post__section_container}>
       <div>
         <h2>Recently in Bloggie</h2>
       </div>
       <section className={styles.post__section__column}>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido</h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido</h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido</h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido</h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido</h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido </h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido </h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido </h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
-        <article>
-          <section className={styles.post__card__writer}>
-            <figure>
-              <img src="" alt="" />
-            </figure>
-            <p>This is the writer</p>
-          </section>
-          <section className={styles.post__card__title}>
-            <h4>This is the post title y algo más de contenido </h4>
-          </section>
-          <section className={styles.post__card__data}>
-            <p className={styles.date}>Posted, Sep 6</p>
-          </section>
-        </article>
+        {
+          post?.map((posts) => (
+            <article key={posts?._id}>
+              <section className={styles.post__card__writer}>
+                <figure>
+                  <img src="" alt="" />
+                </figure>
+                <p>{posts?.writer}</p>
+              </section>
+              <section className={styles.post__card__title}>
+                <h4>{posts?.title}</h4>
+              </section>
+              <section className={styles.post__card__data}>
+                <p className={styles.date}>{posts?.createdAt}</p>
+              </section>
+            </article>
+          ))
+        }
       </section>
     </main>
   )
