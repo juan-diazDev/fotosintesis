9a7526106f3e321cd7fefed702810611f60fb97f
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-19 17:16:26 -0500

    feat: add services

--- services/index.ts
+++ services/index.ts
@@ -1,6 +1,34 @@
-const BASE_URL = process.env.LOCAL_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_LOCAL_URL;
+
+export async function login(auth) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(auth),
+  };
+  const response = await fetch(`${BASE_URL}/auth/local/login`, options);
+  return response.json();
+}
 
 export async function verify(token) {
   const response = await fetch(`${BASE_URL}/auth/local/verifyAccount/${token}`);
   return response.json();
+}
+
+export async function signup(user) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(`${BASE_URL}/api/users/create`, options);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
 }
