80aaeccc2048bc0252624f61789fbbd09c0d12c5
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-18 21:59:29 -0500

    feat: add verification mail when creating user

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -2,12 +2,17 @@
 import crypto from 'crypto';
 import connectDB from '../../../server/config/database';
 import { createUser } from '../../../server/users/user.services';
+import { sendMailSendGrid } from '../../../server/config/email'
 
 export default async (req, res) => {
   await connectDB();
   const userData = req.body;
 
   try {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ '[ERROR]': 'Method not allowed' });
+    }
+
     const hash = crypto.createHash('sha256')
       .update(userData.email)
       .digest('hex');
@@ -16,9 +21,18 @@ export default async (req, res) => {
     userData.passwordResetExpires = Date.now() + 3_600_000 * 24;
     const user = await createUser(userData);
 
-    if (req.method !== 'POST') {
-      return res.status(405).json({ '[ERROR]': 'Method not allowed' });
+    const message = {
+      from: 'Juan de Bloggie <dhruva0108@gmail.com>',
+      to: user.email,
+      subject: 'Activate your Account',
+      template_id: 'd-df1380cc1da24d33ab9f925072819e6f',
+      dynamic_template_data: {
+        firstName: user.firstName,
+        url: `http://localhost:3000/verify-account/${hash}`
+      }
     }
+
+    await sendMailSendGrid(message);
 
     return res.status(200).json({ user });
   } catch (error) {
