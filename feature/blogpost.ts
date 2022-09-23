787495920d37e6951c9768dcb6a6ae2b58c54dcb
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-23 09:21:23 -0500

    feat: modify slice for user and blogpost

--- feature/blogpost.ts
+++ feature/blogpost.ts
@@ -1,13 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  blogpost: null,
+  blogpostList: [],
+
 };
 
 export const blogpostSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-
+    setGetAllPostState: (_, action) => action.payload,
+    setCreatePostState: (_, action) => action.payload,
+    setGetSinglePostState: (_, action) => action.payload,
+    setUpdatePostState: (_, action) => action.payload,
+    setDeletePostState: (_, action) => action.payload,
   }
 });
+
+export const {
+  setGetAllPostState,
+  setCreatePostState,
+  setGetSinglePostState,
+  setUpdatePostState,
+  setDeletePostState,
+} = blogpostSlice.actions;
+
+export default blogpostSlice.reducer;
--- feature/userSlice.ts
+++ feature/userSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
+  loasAllUsers: [],
   userDetail: {},
 };
 
@@ -8,8 +9,22 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-
+    setGetAllUserState: (_, action) => action.payload,
+    setCreateUserState: (_, action) => action.payload,
+    setGetMeState: (state, action) => {
+      state.userDetail = action.payload
     },
+    setUpdateUserState: (_, action) => action.payload,
+    setDeleteUserState: (_, action) => action.payload,
+  },
 });
+
+export const {
+  setGetAllUserState,
+  setCreateUserState,
+  setGetMeState,
+  setUpdateUserState,
+  setDeleteUserState,
+ } = userSlice.actions;
 
 export default userSlice.reducer;
