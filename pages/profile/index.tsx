6041fbc7a31885c49afb50194af20c041eac9177
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-23 23:15:45 -0500

    feat: modify profile page and start design

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import Profile from '../../components/Profile'
+import React from 'react';
+import Sidebar from '../../components/Home/Sidebar';
+import Suggest from '../../components/Home/Suggest';
+import Profile from '../../components/Profile';
+import styles from '../../styles/Profile.module.scss';
 
 const profile = () => {
   return (
-    <Profile />
+    <main className={styles.profile__main_container}>
+      <Sidebar />
+      <Profile />
+      <Suggest />
+    </main>
   )
 }
 
