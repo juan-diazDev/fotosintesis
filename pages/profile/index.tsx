252db1a0ca6c3d8af24e60ca2f13b96ed6697674
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-25 17:52:48 -0500

    feat: add Head tag to profile and create pages

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Sidebar from '../../components/Home/Sidebar';
 import Suggest from '../../components/Home/Suggest';
 import Profile from '../../components/Profile';
@@ -7,6 +8,12 @@ import styles from '../../styles/Profile.module.scss';
 const profile = () => {
   return (
     <main className={styles.profile__main_container}>
+      <Head>
+        <title>This is me</title>
+        <meta name="description" content="See your profile" />
+        <link rel="icon" href="https://res.cloudinary.com/juanito-om/image/upload/v1663682350/Fotos%C3%ADntesis/Logo/Negro_vyop2z.png" />
+      </Head>
+
       <Sidebar />
       <Profile />
       <Suggest />
