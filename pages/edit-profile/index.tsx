042b9efeca87da9074e36455b9b0788a50cb6ce5
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-26 15:30:48 -0500

    feat: add edit-profile page

---
+++ pages/edit-profile/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EditProfile from '../../components/EditProfile';
+
+const CompleteProfile = () => {
+  return (
+    <EditProfile />
+  )
+}
+
+export default CompleteProfile;
