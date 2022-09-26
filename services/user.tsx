97a3b380363ec94d0a4c5478c5deb87942ddc3ee
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-26 15:32:29 -0500

    feat: add user service for updating and upload service for uspload avatar

--- services/user.tsx
+++ services/user.tsx
@@ -15,3 +15,21 @@ export async function getUser() {
     return new Error(error);
   }
 }
+
+export async function updateUser(user) {
+  try {
+    const token = localStorage.getItem('token');
+    const options = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(`${BASE_URL}/api/users/${user.id}`, options);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
+}
